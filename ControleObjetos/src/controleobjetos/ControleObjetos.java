90e929f9114df5f50e27acd746fb9a426e73ce57
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-12 21:04:57 -0300

    Refatoração

--- ControleObjetos/src/controleobjetos/ControleObjetos.java
+++ ControleObjetos/src/controleobjetos/ControleObjetos.java
@@ -4,6 +4,8 @@
  */
 package controleobjetos;
 
+import tela.FrmPrincipal;
+
 /**
  *
  * @author artur
@@ -14,7 +16,7 @@ public class ControleObjetos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
     }
     
 }
