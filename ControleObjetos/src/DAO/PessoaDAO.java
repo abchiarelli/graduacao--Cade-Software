e869b69c2ab52d9482c1f7c9591cbfc4e070413c
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-27 17:05:54 -0300

    Update PessoaDAO
    - Implementado método Atualizar;
    - Implementado método Excluir;
    - Implementado método consutarId;

--- ControleObjetos/src/DAO/PessoaDAO.java
+++ ControleObjetos/src/DAO/PessoaDAO.java
@@ -39,12 +39,41 @@ public class PessoaDAO implements IDAOT<Pessoa> {
 
     @Override
     public String atualizar(Pessoa o) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String dml = "UPDATE pessoa SET "
+                + "nome = '" + o.getNome() + "', "
+                + "apelido = '" + o.getApelido() + "', "
+                + "email = '" + o.getEmail() + "', "
+                + "telefone = '" + o.getTelefone() + "' "
+                + "WHERE id = " + o.getId() + ";";
+        
+        System.out.println("DML alterar Pessoa: " + dml);
+        
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            int retorno = st.executeUpdate(dml);
+
+            return null;
+        } catch (Exception e) {
+            System.out.println("Erro ao alterar: " + e);
+            return e.toString();
+        }
     }
 
     @Override
     public String excluir(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String dml = "DELETE FROM pessoa WHERE id = " + id;
+
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            int rotorno = st.executeUpdate(dml);
+
+            return null;
+        } catch (Exception e) {
+            System.out.println("Erro ao excluir: " + e);
+            return e.toString();
+        }
     }
 
     @Override
@@ -63,9 +92,9 @@ public class PessoaDAO implements IDAOT<Pessoa> {
                 String apelido = rs.getString("apelido");
                 String email = rs.getString("email");
                 String telefone = rs.getString("telefone");
-                
+
                 Pessoa pessoa = new Pessoa(id, nome, apelido, email, telefone);
-                
+
                 pessoas.add(pessoa);
             }
 
@@ -90,9 +119,9 @@ public class PessoaDAO implements IDAOT<Pessoa> {
                 String apelido = rs.getString("apelido");
                 String email = rs.getString("email");
                 String telefone = rs.getString("telefone");
-                
+
                 Pessoa pessoa = new Pessoa(id, nome, apelido, email, telefone);
-                
+
                 pessoas.add(pessoa);
             }
 
@@ -105,7 +134,28 @@ public class PessoaDAO implements IDAOT<Pessoa> {
 
     @Override
     public Pessoa consultarId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Pessoa pessoa = null;
+
+        try {
+            String dml = "SELECT * FROM pessoa ORDER BY nome;";
+
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+            ResultSet rs = st.executeQuery(dml);
+
+            while (rs.next()) {
+                String nome = rs.getString("nome");
+                String apelido = rs.getString("apelido");
+                String email = rs.getString("email");
+                String telefone = rs.getString("telefone");
+
+                pessoa = new Pessoa(id, nome, apelido, email, telefone);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar PESSOA: " + e);
+        }
+
+        return pessoa;
     }
 
 }
