f023034339cc701b117da5251fd3f65414800756
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-30 18:09:25 -0300

    Create CombosDAO.java

---
+++ ControleObjetos/src/apoio/CombosDAO.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apoio;
+
+import java.sql.ResultSet;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author artur
+ */
+public class CombosDAO {
+
+    ResultSet resultado = null;
+
+    public void popularComboBox(String tabela, JComboBox comboBox) {
+        comboBox.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setId(0);
+        item.setDescricao("--- Selecione ---");
+        comboBox.addItem(item);
+
+        try {
+            String dml = "SELECT * FROM "
+                    + tabela + " "
+                    + "ORDER BY 2;";
+
+            resultado = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(dml);
+
+            if (resultado.isBeforeFirst()) {
+                while (resultado.next()) {
+                    item = new ComboItem();
+                    item.setId(resultado.getInt(1));
+                    item.setDescricao(resultado.getString(2));
+
+                    comboBox.addItem(item);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular ComboBox: " + e);
+        }
+    }
+
+    public void popularComboBox(String tabela, JComboBox comboBox, String addQuery) {
+        comboBox.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setId(0);
+        item.setDescricao("--- Selecione ---");
+        comboBox.addItem(item);
+
+        try {
+            String dml = "SELECT * FROM "
+                    + tabela + " "
+                    + addQuery
+                    + "ORDER BY 2;";
+
+            resultado = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(dml);
+
+            if (resultado.isBeforeFirst()) {
+                while (resultado.next()) {
+                    item = new ComboItem();
+                    item.setId(resultado.getInt(1));
+                    item.setDescricao(resultado.getString(2));
+
+                    comboBox.addItem(item);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular ComboBox: " + e);
+        }
+    }
+    
+    public void definirItemCombo(JComboBox combo, int id) {
+        for (int i = 0; i < combo.getItemCount(); i++) {
+            if (((ComboItem) combo.getItemAt(i)).getId() == id) {
+                combo.setSelectedIndex(i);
+                return;
+            }
+        }
+    }
+}
