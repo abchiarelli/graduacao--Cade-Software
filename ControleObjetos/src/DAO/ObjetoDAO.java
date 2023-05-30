1d3cf8e87b260bfdef44ab53941e3a220a35319b
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-30 18:10:25 -0300

    Update ObjetoDAO.java

--- ControleObjetos/src/DAO/ObjetoDAO.java
+++ ControleObjetos/src/DAO/ObjetoDAO.java
@@ -69,9 +69,9 @@ public class ObjetoDAO implements IDAOT<Objeto> {
                 String publisher = rs.getString("publisher");
                 int status = rs.getInt("status_id");
                 int tipo = rs.getInt("tipo_objeto_id");
-                
+
                 Objeto objeto = new Objeto(id, titulo, autor, publisher, status, tipo);
-                
+
                 objetos.add(objeto);
             }
         } catch (Exception e) {
@@ -85,21 +85,21 @@ public class ObjetoDAO implements IDAOT<Objeto> {
         ArrayList<Objeto> objetos = new ArrayList<>();
 
         try {
-            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+            ResultSet rs = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(criterio);
 
-            ResultSet rs = st.executeQuery(criterio);
+            if (rs.isBeforeFirst()) {
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String titulo = rs.getString("titulo");
+                    String autor = rs.getString("autor");
+                    String publisher = rs.getString("publisher");
+                    int status = rs.getInt("status_id");
+                    int tipo = rs.getInt("tipo_objeto_id");
 
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String titulo = rs.getString("titulo");
-                String autor = rs.getString("autor");
-                String publisher = rs.getString("publisher");
-                int status = rs.getInt("status_id");
-                int tipo = rs.getInt("tipo_objeto_id");
-                
-                Objeto objeto = new Objeto(id, titulo, autor, publisher, status, tipo);
-                
-                objetos.add(objeto);
+                    Objeto objeto = new Objeto(id, titulo, autor, publisher, status, tipo);
+
+                    objetos.add(objeto);
+                }
             }
         } catch (Exception e) {
         }
